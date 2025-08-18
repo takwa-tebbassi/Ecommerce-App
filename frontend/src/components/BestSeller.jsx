3f9ad44a9737694b150a6085b488b7ed69f1177d
Takwa Tebbassi <tebbassi.takoua@gmail.com>
2025-08-18 20:33:56 +0200

    update files

---
+++ frontend/src/components/BestSeller.jsx
@@ -0,0 +1,27 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
+
+const BestSeller = () => {
+    const {products} = useContext(ShopContext);
+    const [BestSeller, setBestSeller] = useState([]);
+
+    useEffect(()=>{
+        const bestProduct = products.filter((item)=>(item.bestseller));
+        setBestSeller(bestProduct.slice(0,5))
+    },[])
+  return (
+    <div className='my-10'>
+        <div className='text-center text-3xl py-8'>
+            <Title text1={'BEST'} text2={'SELLERS'} />
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+            Lorem Ipsum is simply dummy text of the printing and typescripting industry.
+            </p>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default BestSeller
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
 import {assets} from '../assets/assets'
 
 const Navbar = () => {
+
+  const [visible,setVisible] = useState(false);
+
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
 
@@ -35,6 +38,37 @@ const Navbar = () => {
 
           <div className='group relative'>
             <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
+            <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+              <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                <p className='curser-pointer hover:text-black'>My Profile</p>
+                <p className='curser-pointer hover:text-black'>Orders</p>
+                <p className='curser-pointer hover:text-black'>Logout</p>
+              </div>
+            </div>
+          </div>
+
+          <Link to='/cart' className='relative'>
+            <img src={assets.cart_icon} className='w-5 min-w-5' alt='' />
+            <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+          </Link>
+
+          <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt='' />
+
+        </div>
+
+        {/* sidebar menu for small screen */}
+
+        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'} `} >
+          <div className='flex flex-col text-gray-600'>
+            <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+              <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
+              <p>Back</p>
+            </div>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/colection'>COLLECTION</NavLink>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border'to='/about'>ABOUT</NavLink>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>Contact</NavLink>
+
           </div>
         </div>
 
